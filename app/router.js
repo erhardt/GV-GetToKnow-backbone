697c02460fa8126dfb2e7e7dbd50b310d79a8681
erhardt <erhardt@gmail.com>
2012-12-04 23:47:41 -0500

    Fixed error where history didn't work on original index.html page

--- app/router.js
+++ app/router.js
@@ -2,18 +2,24 @@ var GVRouter = Backbone.Router.extend({
   
   routes: {
     ":page": "updateCountry",
+    "*path": "resetIndex",
   },
 
   viewCountry: function(id){
-    console.log("View country requested:" + id);
+    console.log("View country requested: " + id);
     this.navigate(id, true); // updates the fragment and triggers the route as well
   },
 
   updateCountry: function(page){
-    if (page != null){
-      $('#gv-country').val(page);
-      splashView.lookupCountry();
-    }
+    console.log("Returning to country: " + page);
+    $('#gv-country').val(page);
+    splashView.lookupCountry();
+  },
+
+  resetIndex: function(path){
+    console.log("Reset page to index");
+    $('#gv-country').val("");
+    splashView.initialize();
   },
 });
 
